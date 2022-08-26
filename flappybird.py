30ab6ad4778e674b0b8e7032a1a50685eee29bbd
MinimiseOCE <jackson.whiteau@gmail.com>
2022-08-26 15:39:57 +0800

    Procedurally delete and add pipes

--- flappybird.py
+++ flappybird.py
@@ -162,16 +162,23 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 # Draws the game sprites
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base):
     win.blit(BG_IMG, (0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 # Main Game Function
 def main():
-    bird = Bird(200,200)
+    bird = Bird(230,350)
+    base = Base(730)
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
     clock = pygame.time.Clock()
+    score = 0
 
     run = True
     while run:
@@ -181,7 +188,31 @@ def main():
                 run = False
 
         bird.move()
-        draw_window(win, bird)
+        add_pipe = False
+        rem = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+            
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+
+
+        base.move()
+        draw_window(win, bird, pipes, base)
 
     pygame.quit()
     quit()
